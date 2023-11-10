d729cec7c3526e6f1c1caab74fdbf2486f033453
Tahir Bastu Abiodun <absullahabeyking@gmail.com>
2023-11-10 02:47:59 -0800

    Add User Information

--- UserInfo.cs
+++ UserInfo.cs
@@ -5,7 +5,7 @@ namespace BankApplication
         private string IDNumber;
         private string Address;
         private string Occupation;
-        public string LastName { get; set; }
+        public string LastName { get; set; } 
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string PhoneNumber { get; set; }
